f5c44d974100c1a0ffcb25adf840cd9d8d9fe6f4
sambanihit98 <sambanihit@gmail.com>
2026-04-21 11:45:09 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ function lin_enqueue_assets()
 {
     // Styles
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('main', get_template_directory_uri() . '/main.css');
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 
     // Google Fonts
